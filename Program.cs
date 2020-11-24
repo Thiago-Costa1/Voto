441f33eb1325a4c2494d50a39daed0d716110749
Thiago <thiagocostas2017@gmail.com>
2020-11-24 09:50:45 -0300

    completo

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Votar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+         Console.WriteLine("Escreva a sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            int media( int idade)
+            {
+                int voto = idade - 2020;
+
+                return voto;
+            }
+
+            if( media(idade) >= 16)
+            { Console.WriteLine($"Parabéns, você pode votar! ");   }
+             else if ( media(idade) <= 15)
+            {  Console.WriteLine($"Sinto muito, mas você não tem a idade minima para votar:");   }
+        
+        }
+    }
+}
+   
+
